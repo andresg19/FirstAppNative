30e1540a98f0789a96423057ee19ede18fe3bbdf
andresg19 <andygermain17@gmail.com>
2023-06-20 15:10:06 -0300

    First App Native

---
+++ src/Redux/Reducer/index.js
@@ -0,0 +1,11 @@
+const initialState = {
+   
+};
+
+export default function rootReducer(state = initialState, { type, payload }) {
+    switch (type) {
+       
+        default:
+            return state;
+    }
+}
