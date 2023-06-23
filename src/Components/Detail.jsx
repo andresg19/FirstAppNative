44e21f928eb21114fb3d8ecc4a90318bfa4bf3b5
andresg19 <andygermain17@gmail.com>
2023-06-23 14:50:33 -0300

    get detail

--- src/Components/Detail.jsx
+++ src/Components/Detail.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useRoute } from '@react-navigation/native';
+import { getDetail } from '../Redux/Actions/actions';
 
 export default function Detail () {
+    const route = useRoute();
+    const { name } = route.params;
+    const dispatch = useDispatch();
     const data = useSelector(state => state.detail);
     console.log(data);
+
+    useEffect(() => {
+        dispatch(getDetail(name));
+    }, []);
     return (
         <View>
             <Text>Hola!</Text>
--- src/Components/ViewLists.jsx
+++ src/Components/ViewLists.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Text, TouchableOpacity, View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { getDetail, getTables } from '../Redux/Actions/actions';
@@ -8,21 +8,25 @@ import { ScrollView } from 'react-native-gesture-handler';
 
 export default function ViewLists ({navigation}) {
     const nameLists = useSelector(state => state.componentAllLists[0]);
+    const [press, setPress] = useState(false);
+
     console.log(nameLists)
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getTables());
-       }, [])
-
-       const onTouch = (name) => {
-       } 
+    }, [])
+    
+   const handlePress = (name) => {
+    navigation.navigate("Detalle", {name:name} )
+   }
+   
     return ( 
         <View style={styles.container}>
        <ScrollView contentContainerStyle={styles.scrollContent}>
         { nameLists ? nameLists.map((name, i) => (
             <View key={i} style={styles.content}> 
-            <Button  style={styles.btn} title={`${name}`} />
+            <Button  style={styles.btn} onPress={()=> handlePress(name)} title={`${name}`} />
             </View>
         )) : null}
        </ScrollView>
